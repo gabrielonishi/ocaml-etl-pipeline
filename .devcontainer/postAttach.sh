2b7c74f5ead0444880d27d62e664d972439a01e3
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-02-23 11:49:35 -0300

    build(bashrc): adds opam config to bashrc

--- .devcontainer/postAttach.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-opam install etl/ --deps-only -y
-eval $(opam env)
--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,3 +2,5 @@
 opam init -y
 opam install etl/ --deps-only -y
 eval $(opam env)
+echo 'opam install . --deps-only -y' >> ~/.bashrc
+echo 'eval $(opam env)' >> ~/.bashrc
