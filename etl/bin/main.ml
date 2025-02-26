90064791bb9b67717f76bc300a79a35dd773df88
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-02-25 22:37:43 -0300

    feat(join): outer joins the two record lists

--- etl/bin/main.ml
+++ etl/bin/main.ml
@@ -1,12 +1,18 @@
 open Lib.Helpers
 
-(* let order_data = Lib.Read.read_order ;;
-let orders = Lib.Helpers.load_order_records order_data ;; *)
+let order_data = Lib.Read.read_order ;;
+let orders = load_order_records order_data ;;
 
-(* let item_data = Lib.Read.read_item ;;
-let items = Lib.Helpers.load_item_records item_data ;;
+let item_data = Lib.Read.read_item ;;
+let items = load_item_records item_data ;;
+
+let t = join_order_item orders items ;;
 
 let () = List.iter (
+  fun x -> Printf.printf "%d %d\n" x.order_id x.product_id
+  ) t ;;
+
+(* let () = List.iter (
   fun x -> Printf.printf "%f\n" x.tax
   ) items ;; *)
 
--- etl/lib/pure/helpers.ml
+++ etl/lib/pure/helpers.ml
@@ -3,7 +3,7 @@ type order = {
   client_id: int;
   order_date: string;
   status: string;
-  origin: string
+  origin: string;
 } ;;
 
 type item = {
@@ -14,36 +14,65 @@ type item = {
   tax: float;
 } ;;
 
-let load_order_records order_data =
-  let data = List.tl order_data in
-    let records =
-      List.fold_left (fun acc order ->
-        match order with
-        | [a; b; c; d; e] ->
-            let this_order = {
-              id = int_of_string a;
-              client_id = int_of_string b;
-              order_date = c;
-              status = d;
-              origin = e;
-            } in
-            this_order :: acc
-        | _ -> failwith "Unexpected number of items in row"
-      ) [] data in records ;;
+type order_item = {
+  order_id: int;
+  product_id: int;
+  quantity: int;
+  price: float;
+  tax: float;
+  client_id: int;
+  order_date: string;
+  status: string;
+  origin: string;
+} ;;
 
-let load_item_records item_data =
-  let data = List.tl item_data in
-    let records =
-      List.fold_left (fun acc item ->
-        match item with
-        | [a; b; c; d; e] ->
-            let this_order = {
-              order_id = int_of_string a;
-              product_id = int_of_string b;
-              quantity = int_of_string c;
-              price = float_of_string d;
-              tax = float_of_string e;
-            } in
-            this_order :: acc
-        | _ -> failwith "Unexpected number of items in row"
-      ) [] data in records ;;
+let load_order_records (order_data : string list list) : order list =
+  let csv_data = List.tl order_data in
+  List.fold_left (fun acc this_order ->
+    match this_order with
+    | [a; b; c; d; e] ->
+        {
+          id = int_of_string a;
+          client_id = int_of_string b;
+          order_date = c;
+          status = d;
+          origin = e;
+        } :: acc
+    | _ -> failwith "Unexpected number of items in row"
+  ) [] csv_data ;;
+
+let load_item_records (item_data : string list list) : item list =
+  let csv_data = List.tl item_data in
+  List.fold_left (fun acc this_item ->
+    match this_item with
+    | [a; b; c; d; e] ->
+        {
+          order_id = int_of_string a;
+          product_id = int_of_string b;
+          quantity = int_of_string c;
+          price = float_of_string d;
+          tax = float_of_string e;
+        } :: acc
+    | _ -> failwith "Unexpected number of items in row"
+  ) [] csv_data ;;
+
+let product_map (this_order : order) (acc : order_item list) (this_item : item) : order_item list =
+  if this_item.order_id = this_order.id then
+    {
+      order_id = this_item.order_id;
+      product_id = this_item.product_id;
+      quantity = this_item.quantity;
+      price = this_item.price;
+      tax = this_item.tax;
+      client_id = this_order.client_id;
+      order_date = this_order.order_date;
+      status = this_order.status;
+      origin = this_order.origin;
+    } :: acc
+  else acc
+
+let order_map (items: item list) (acc : order_item list) (this_order : order) : order_item list =
+  List.fold_left (product_map this_order) acc items ;;
+
+let join_order_item (orders : order list) (items : item list) : order_item list =
+  List.fold_left (order_map items) [] orders ;;
