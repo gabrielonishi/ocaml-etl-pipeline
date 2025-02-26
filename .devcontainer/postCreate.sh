dc132479752a60b459905f833e932ab7439314fb
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-02-26 12:10:05 -0300

    build(devcontainer): adds ocamlformat install to container

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-opam init -y
-opam install etl/ --deps-only -y
+opam init -y && \
 eval $(opam env)
+opam install etl/ --deps-only -y
+opam install -y ocamlformat user-setup
+opam user-setup install
 echo 'opam install . --deps-only -y' >> ~/.bashrc
 echo 'eval $(opam env)' >> ~/.bashrc
