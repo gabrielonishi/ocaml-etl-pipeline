234124a81501930084709e606f9b4203e0bafeac
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-03-13 16:30:58 -0300

    build(devcontainer): changes devcontainer to work with OCaml Platform vscode extension

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,5 @@
-opam init -y && \
-eval $(opam env)
+opam init --compiler=5.2.0 --disable-sandboxing -y
+echo "test -r /home/vscode/.opam/opam-init/init.sh && . /home/vscode/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true" >> ~/.bashrc
 opam install etl/ --deps-only -y
-opam install -y ocamlformat user-setup
+opam install -y ocamlformat user-setup ocaml-lsp-server utop
 opam user-setup install
-echo 'opam install . --deps-only -y' >> ~/.bashrc
-echo 'eval $(opam env)' >> ~/.bashrc
