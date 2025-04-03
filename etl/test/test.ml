e1dd101a39ad8f1ac3c188844d93aa97102b3da3
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-04-03 18:18:35 -0300

    test(group_by_order_id): tests function

--- etl/test/test.ml
+++ etl/test/test.ml
@@ -7,6 +7,7 @@ let () =
       ("load_order_item_suite", Test_csv_parser.load_order_item_suite);
       ("build_csv_output_suite", Test_csv_parser.build_csv_output_suite);
       ("group_by", Test_process.group_by_suite);
+      ("group_by_order_id", Test_process.group_by_order_id_suite);
       (* ("int_is_in_suite", Test_process.int_is_in_suite);
       ("process_order_suite", Test_process.process_order_suite);
       ("group_by_ids_suite", Test_process.group_by_ids_suite);
--- etl/test/test_process.ml
+++ etl/test/test_process.ml
@@ -2,10 +2,10 @@ open Lib.Schemas
 open Lib.Process
 open Testables
 
-let test_group_by_group_items_by_product_id (data : 'a list)
-    (expected : 'b IntMap.t) (key_extractor : 'a -> int)
-    (value_extractor : 'a -> 'b) (aggregate_handler : 'b -> 'b -> 'b)
-    (value_testable : 'b Alcotest.testable) () =
+let test_group_by (data : 'a list) (expected : 'b IntMap.t)
+    (key_extractor : 'a -> int) (value_extractor : 'a -> 'b)
+    (aggregate_handler : 'b -> 'b -> 'b) (value_testable : 'b Alcotest.testable)
+    () =
   let result = group_by key_extractor value_extractor aggregate_handler data in
   Alcotest.(check (map_testable value_testable)) "group_by" expected result
 
@@ -43,8 +43,8 @@ let group_by_suite =
           tax = acc.tax +. current_item.tax;
         }
       in
-      test_group_by_group_items_by_product_id items expected key_extractor
-        value_extractor aggregate_handler item_testable );
+      test_group_by items expected key_extractor value_extractor
+        aggregate_handler item_testable );
     ( "Group int",
       `Quick,
       let expected = IntMap.empty |> IntMap.add 100 13 in
@@ -53,249 +53,37 @@ let group_by_suite =
       let aggregate_handler (acc : int) (current_item : int) : int =
         acc + current_item
       in
-      test_group_by_group_items_by_product_id items expected key_extractor
-        value_extractor aggregate_handler Alcotest.int );
-  ]
-(* let test_group_by_ids (order_items : order_item list)
-    (expected : order_total list) () =
-  Alcotest.(check (list order_summary_testable))
-    "group_by_ids" expected (group_by_ids order_items)
-
-let group_by_ids_suite =
-  [
-    ("group_by_ids empty list", `Quick, test_group_by_ids [] []);
-    ( "group_by_ids single item",
-      `Quick,
-      test_group_by_ids
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        [
-          {
-            order_id = 1;
-            total_amount = 12. *. 10.4;
-            total_taxes = 12. *. 10.4 *. 0.2;
-          };
-        ] );
-    ( "group_by_ids multiple items same order",
-      `Quick,
-      test_group_by_ids
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 1;
-            product_id = 101;
-            quantity = 1;
-            price = 50.;
-            tax = 0.1;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        [
-          {
-            order_id = 1;
-            total_amount = (12. *. 10.4) +. (1. *. 50.);
-            total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-          };
-        ] );
-    ( "group_by_ids multiple items different orders",
-      `Quick,
-      test_group_by_ids
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 2;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 1;
-            product_id = 101;
-            quantity = 1;
-            price = 50.;
-            tax = 0.1;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        [
-          {
-            order_id = 1;
-            total_amount = (12. *. 10.4) +. (1. *. 50.);
-            total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-          };
-          {
-            order_id = 2;
-            total_amount = 12. *. 10.4;
-            total_taxes = 12. *. 10.4 *. 0.2;
-          };
-        ] );
+      test_group_by items expected key_extractor value_extractor
+        aggregate_handler Alcotest.int );
   ]
 
-let test_build_output (order_items : order_item list) (status : string)
-    (origin : string) (expected : order_total list) () =
-  Alcotest.(check (list order_summary_testable))
-    "build_output" expected
-    (build_output order_items status origin)
+let group_by_order_id (order_items : order_item list) : order_total IntMap.t =
+  let key_extractor (order_item : order_item) : int = order_item.order_id in
+  let value_extractor (order_item : order_item) : order_total =
+    {
+      order_id = order_item.order_id;
+      total_amount = float_of_int order_item.quantity *. order_item.price;
+      total_taxes =
+        float_of_int order_item.quantity *. order_item.price *. order_item.tax;
+    }
+  in
+  let aggregate_handler (acc : order_total)
+      (current_order_summary : order_total) : order_total =
+    {
+      order_id = acc.order_id;
+      total_amount = acc.total_amount +. current_order_summary.total_amount;
+      total_taxes = acc.total_taxes +. current_order_summary.total_taxes;
+    }
+  in
+  group_by key_extractor value_extractor aggregate_handler order_items
 
-let build_output_suite =
-  [
-    ("build_output empty list", `Quick, test_build_output [] "Pending" "O" []);
-    ( "build_output single item",
-      `Quick,
-      test_build_output
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        "Pending" "O"
-        [
-          {
-            order_id = 1;
-            total_amount = 12. *. 10.4;
-            total_taxes = 12. *. 10.4 *. 0.2;
-          };
-        ] );
-    ( "build_output multiple items same order",
-      `Quick,
-      test_build_output
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 1;
-            product_id = 101;
-            quantity = 1;
-            price = 50.;
-            tax = 0.1;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        "Pending" "O"
-        [
-          {
-            order_id = 1;
-            total_amount = (12. *. 10.4) +. (1. *. 50.);
-            total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-          };
-        ] );
-    ( "build_output multiple items different orders",
-      `Quick,
-      test_build_output
-        [
-          {
-            order_id = 1;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 2;
-            product_id = 100;
-            quantity = 12;
-            price = 10.4;
-            tax = 0.2;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-          {
-            order_id = 1;
-            product_id = 101;
-            quantity = 1;
-            price = 50.;
-            tax = 0.1;
-            client_id = 100;
-            order_date = "2024-10-02T03:05:39";
-            status = "Pending";
-            origin = "O";
-          };
-        ]
-        "Pending" "O"
-        [
-          {
-            order_id = 1;
-            total_amount = (12. *. 10.4) +. (1. *. 50.);
-            total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-          };
-          {
-            order_id = 2;
-            total_amount = 12. *. 10.4;
-            total_taxes = 12. *. 10.4 *. 0.2;
-          };
-        ] );
-  ] *)
+let test_group_by_order_id (data : order_item list)
+    (expected : order_total IntMap.t) () =
+  let result = group_by_order_id data in
+  Alcotest.(check (map_testable order_total_testable))
+    "group_by_order_id" expected result
 
-(* open Lib.Process
-let unrepeated_ids_order_items =
+let order_items =
   [
     {
       order_id = 1;
@@ -303,10 +91,10 @@ let unrepeated_ids_order_items =
       quantity = 12;
       price = 10.4;
       tax = 0.2;
-      client_id = 100;
-      order_date = "2024-10-02T03:05:39";
-      status = "Pending";
-      origin = "O";
+      client_id = 1;
+      order_date = "2023-01-01";
+      status = "completed";
+      origin = "online";
     };
     {
       order_id = 1;
@@ -314,53 +102,44 @@ let unrepeated_ids_order_items =
       quantity = 1;
       price = 50.;
       tax = 0.1;
-      client_id = 100;
-      order_date = "2024-10-02T03:05:39";
-      status = "Pending";
-      origin = "O";
+      client_id = 1;
+      order_date = "2023-01-01";
+      status = "completed";
+      origin = "online";
+    };
+    {
+      order_id = 2;
+      product_id = 100;
+      quantity = 1;
+      price = 50.;
+      tax = 0.1;
+      client_id = 2;
+      order_date = "2023-01-02";
+      status = "completed";
+      origin = "online";
     };
   ]
 
-let order_items =
-  {
-    order_id = 2;
-    product_id = 100;
-    quantity = 12;
-    price = 10.4;
-    tax = 0.2;
-    client_id = 100;
-    order_date = "2024-10-02T03:05:39";
-    status = "Pending";
-    origin = "O";
-  }
-  :: unrepeated_ids_order_items
-
-let%test "process_order" =
-  let res = process_order unrepeated_ids_order_items in
-  let exp =
-    {
-      order_id = 1;
-      total_amount = (12. *. 10.4) +. (1. *. 50.);
-      total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-    }
-  in
-  res = exp
-
-(*group_by_ids*)
-let%test "group_by_ids" =
-  let res = group_by_ids order_items in
-  let exp =
-    [
-      {
-        order_id = 2;
-        total_amount = 12. *. 10.4;
-        total_taxes = 12. *. 10.4 *. 0.2;
-      };
-      {
-        order_id = 1;
-        total_amount = (12. *. 10.4) +. (1. *. 50.);
-        total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
-      };
-    ]
-  in
-  res = exp *)
+let group_by_order_id_suite =
+  [
+    ( "Group by order_id",
+      `Quick,
+      let expected =
+        IntMap.of_list
+          [
+            ( 1,
+              {
+                order_id = 1;
+                total_amount = (12. *. 10.4) +. (1. *. 50.);
+                total_taxes = (12. *. 10.4 *. 0.2) +. (1. *. 50. *. 0.1);
+              } );
+            ( 2,
+              {
+                order_id = 2;
+                total_amount = 1. *. 50.;
+                total_taxes = 1. *. 50. *. 0.1;
+              } );
+          ]
+      in
+      test_group_by_order_id order_items expected );
+  ]
